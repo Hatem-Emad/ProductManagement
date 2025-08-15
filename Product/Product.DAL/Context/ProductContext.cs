df896627d0da96fc076c86059dfea7379f3d10a9
Hatem-Emad <hatememadelden@gmail.com>
2025-08-15 20:12:46 +0200

    Adding Product.DAL Project

---
+++ Product/Product.DAL/Context/ProductContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.DAL.Entities;
+
+namespace Product.DAL.Context
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Item> Item { get; set; }
+        public virtual DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Item>()
+            //    .HasOne(i => i.Category)
+            //    .WithMany()
+            //    .HasForeignKey(x => x.CategoryId)
+            //    .HasPrincipalKey(c => c.ID);
+        }
+    }
+}
